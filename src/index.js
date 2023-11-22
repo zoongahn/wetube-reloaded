6c3eb3086a153af5817b7b9a77f17c97c3fbf1dc
zoongahn <anjungh0103@gmail.com>
2023-11-22 23:21:09 +0900

    CH3.6 middleware 231122

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url == "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed. You may continue.");
     next();
 };
 
@@ -13,14 +22,14 @@ const handleHome = (req, res) => {
     return res.send("I love middlewares");
 };
 
-const handleLogin = (req, res) => {
-    return res.send({ message: "Login here" });
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
 };
 
-app.use(gossipMiddleware);
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
-
+app.get("/protected", handleProtected);
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
