1fde7ad489e5108f9ac7f67754cffafa46f7680f
joonghyunahn <anjungh0103@gmail.com>
2023-12-20 16:58:10 +0900

    move to webstorm

--- src/server.js
+++ src/server.js
@@ -1,24 +1,20 @@
-import express from "express"
-import morgan from "morgan"
-import globalRouter from "./routers/globalRouter"
-import userRouter from "./routers/userRouter"
-import videoRouter from "./routers/videoRouter"
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000
+const app = express();
+const logger = morgan("dev");
 
-const app = express()
-const logger = morgan("dev")
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "pug")
-app.set("views", process.cwd() + "/src/views")
-app.use(logger)
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
-app.use(express.urlencoded({ extended: true }))
-
-app.use("/", globalRouter)
-app.use("/users", userRouter)
-app.use("/videos", videoRouter)
-
-app.listen(PORT, handleListening)
+export default app;
